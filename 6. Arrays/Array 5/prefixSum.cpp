2f43cd5a22df109ac9d8e5ab8af577d57e81d592
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2023-07-25 20:02:05 +0530

    programs

--- 6. Arrays/Array 5/prefixSum.cpp
+++ 6. Arrays/Array 5/prefixSum.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 using namespace std;
 
+void prefixSum(int arr[],int n){
+    for(int i=1; i<n; i++){
+        arr[i] += arr[i-1];
+    }
+}
+
 int main() {
     int n;
     cout<<"Enter n: ";
@@ -11,19 +17,21 @@ int main() {
         cin>>arr[i];
     }
 
-     int sum=0;
-    for(int i=0; i<n; i++){
-        if(i==0){
-            sum+=arr[i];
-            continue;
-        }
-        else{
-            for(int j=i; j<=i; j++){
-                sum+=arr[i];
-            }
-        }
-        arr[i]=sum;
-    }
+    //  int sum=0;
+    // for(int i=0; i<n; i++){
+    //     if(i==0){
+    //         sum+=arr[i];
+    //         continue;
+    //     }
+    //     else{
+    //         for(int j=i; j<=i; j++){
+    //             sum+=arr[i];
+    //         }
+    //     }
+    //     arr[i]=sum;
+    // }
+    
+    prefixSum(arr,n);
     
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";

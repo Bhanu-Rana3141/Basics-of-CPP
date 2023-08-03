ac64b4e3c3f81450c626b014f69b92aaffb783d7
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2023-08-03 22:48:01 +0530

    programs

---
+++ 7.Binary Search/firstLastOcc_using_binarySearch.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+int firstOccurence(int arr[],int n,int key){
+    int start = 0;
+    int end=n-1;
+    int mid = start+(end-start)/2;
+
+    int ans=-1;
+    while(start<=end){
+        if(arr[mid] == key){
+            ans=mid;
+            end=mid-1;
+        }
+        else if(arr[mid]>key){
+            end = mid-1;
+        }
+        else if(arr[mid]<key){
+            start=mid+1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+}
+
+int lastOccurence(int arr[] , int n , int key){
+    int start=0;
+    int end = n-1;
+    int mid = start+(end-start)/2;
+    int ans=-1;
+
+    while(start<=end){
+        if(arr[mid] == key){
+            ans=mid;
+            start=mid+1;
+        }
+        else if(arr[mid] > key){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+        mid = start+(end-start)/2;
+    }
+    return ans;
+}
+
+
+int main() {
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int key;
+    cout<<"Enter key: ";
+    cin>>key;
+
+    int start = firstOccurence(arr,n,key);
+    int last = lastOccurence(arr,n,key);
+
+    cout<<"first: "<<start<<endl<<"last : "<<last;
+    
+
+}

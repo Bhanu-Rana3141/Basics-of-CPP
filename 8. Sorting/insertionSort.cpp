961fb5d2d7b267b9573c4e955b6abd787586eb8e
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2023-09-12 17:11:47 +0530

    CPP Programs

---
+++ 8. Sorting/insertionSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[] , int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void sortArr(int arr[] , int n){
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+        int j=i-1;
+        for(; j>=0; j--){
+            if(arr[j] > temp){
+                arr[j+1] = arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1] = temp;
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    sortArr(arr,n);
+    printArray(arr,n);
+    return 0;
+}

eb7b0e0dead742e3d511d96ef1668ff8237549a4
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2023-09-13 13:05:40 +0530

    CPP Programs

--- 8. Sorting/bubbleSort.cpp
+++ 8. Sorting/bubbleSort.cpp
@@ -24,7 +24,6 @@ int sortArr(int arr[], int n){
 void optimised_sort(int arr[], int n){
      for(int i=1; i<n; i++){
         bool flag = false;
-
         for(int j=0; j<n-i; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j],arr[j+1]);
--- 8. Sorting/insertionSort.cpp
+++ 8. Sorting/insertionSort.cpp
@@ -23,6 +23,22 @@ void sortArr(int arr[] , int n){
     }
 }
 
+void sortUsingWhileLoop(int arr[] , int n){
+    for(int i=1; i<n; i++){
+        int temp = arr[i];
+        int j = i-1;
+        while(j>=0){
+            if(arr[j] > temp){
+                arr[j+1] = arr[j];
+            }else{
+                break;
+            }
+           j--;
+        }
+        arr[j+1] = temp;
+    }
+}
+
 int main() {
     int n;
     cout<<"Enter n: ";
@@ -32,8 +48,8 @@ int main() {
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-
-    sortArr(arr,n);
+    // sortArr(arr,n);
+    sortUsingWhileLoop(arr,n);
     printArray(arr,n);
     return 0;
 }

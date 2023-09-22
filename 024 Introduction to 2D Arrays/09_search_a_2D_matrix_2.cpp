ef67a1171d73a8129f3aed0a589c42f5186dfd9d
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2023-09-22 18:36:06 +0530

    Solving questions on 2D Array

---
+++ 024 Introduction to 2D Arrays/09_search_a_2D_matrix_2.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/* Binary search in each row (Function Declaration)
+bool binary_search(int arr[], int target, int rowSize){
+
+    int s=0;
+    int e=rowSize-1;
+    int mid = s+(e-s)/2;
+
+    while(s<=e) {
+        if(arr[mid] == target) {
+            return true;
+        }
+        else if(arr[mid] > target) {
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return false;
+}
+*/
+
+int main() {
+
+    int n; //row size;
+    cout<<"Enter row size: ";
+    cin>>n;
+
+    int m; //col size
+    cout<<"Enter col size: ";
+    cin>>m;
+
+    int arr[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout << endl;
+
+    int target;
+    cout<<"Enter target: ";
+    cin>>target;
+
+/*Approach 1 :  Brute Force Approach ->  linear search --> O(m*n)
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if(arr[i][j] == target) {
+                cout<<"true";
+                return 0;
+            }
+        }
+    }
+    cout<<"False";
+*/
+   
+/*Approach 2 :  Better Aproach -> Using Binary Search in each row as all the rows are sorted
+    int ans = -1;
+    for(int i=0; i<n; i++) {
+       ans = binary_search(arr[i] , target, n);
+       if(ans == 1) {
+        cout<<"true";
+        return 0;
+        }
+    }
+    cout<<"false";
+*/   
+
+// Approach 3: Optimal Solution -> Using sorted rows and cols
+
+    int row=0;
+    int col = m-1;
+    bool flag = false;
+    while(row<n && col>=0) {
+        int element = arr[row][col];
+        if(element == target){
+            flag = true;
+        }
+        else if(element<target) {
+            row++;
+        }
+        else{
+            col++;
+        }
+    }
+
+    flag==true ? cout<<true : cout<<false;
+
+    return 0;
+}

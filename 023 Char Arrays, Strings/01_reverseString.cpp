87aaa9d868dbebf50c8a41bab9dea6d9fa9971e2
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2023-09-25 23:45:12 +0530

    Solving questions on Strings

---
+++ 023 Char Arrays, Strings/01_reverseString.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void reverse(char name[],int n){
+    int s=0;
+    int e=n-1;
+
+    while(s <= e){
+        swap(name[s++], name[e--]);
+    }
+}
+
+int getLength(char name[]){
+    int count = 0;
+    for(int i=0; name[i] != '\0'; i++){
+        count++;
+    }
+    return count;
+}
+
+int main() {
+   
+    cout<<"Enter string: ";
+    char name[20];
+    cin>>name;
+
+    int length = getLength(name);
+    cout<<"Lenght: "<<length<<endl;
+    
+    // Using inbuilt function
+    // reverse(name,name+length);
+    // cout << "name: "<<name;
+    
+    reverse(name,length);
+    cout<<"name: "<<name;
+    return 0;
+}

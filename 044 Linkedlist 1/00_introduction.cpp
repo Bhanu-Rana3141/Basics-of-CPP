e569d8c944f9c43b339650c4e44d4768757beeb9
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2024-02-04 22:30:10 +0530

    cpp programs

--- 044 Linkedlist 1/00_introduction.cpp
+++ 044 Linkedlist 1/00_introduction.cpp
@@ -1,3 +1,14 @@
 /*
+LINKED LIST
 
+* linear data structure
+* collection of nodes, and a node consists of data and address of next node
+* dynamic - insertion/deletion
+* Non - Contiguous memory allocation
+
+- Hare and Tortoise Approach
+- Floyd's Detection Algo
+
+
+Q. Dummy nodes space complexity ? - CONSTANT
 */

7b0f1ef018e6e2284f845cdfd962da78a77f101f
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2024-04-12 23:18:54 +0530

    cpp programs

--- 085 Introduction to graphs/1_implementation.cpp
+++ 085 Introduction to graphs/1_implementation.cpp
@@ -1,28 +1,26 @@
 #include<iostream>
 #include<unordered_map>
-#include<list>
+#include<vector>
 using namespace std;
 
 class Graph {
 
     public:
-        unordered_map<int, list<int>> adj;
+        unordered_map<int, vector<int>> adj;
 
     void addEdge(int u, int v, bool direction) {
-        // direction 0 - represents undirected graph
-        // direction 1 - represents directed graph
-        adj[u].push_back(v);
 
-        if(direction == 0) {
+        adj[u].push_back(v);
+        if(!direction) {
             adj[v].push_back(u);
         }
-    }
+    }    
 
     void print() {
 
         for(auto i : adj) {
             cout << i.first << " -> ";
-            for(auto  j : i.second) {
+            for(auto j : i.second) {
                 cout << j << " ";
             }
             cout << endl;
@@ -30,30 +28,28 @@ class Graph {
     }
 };
 
-int main() {
-
+int main()
+{
+    
     int n;
-    cout << "Enter no of nodes: ";
+    cout << "Enter number of nodes: ";
     cin >> n;
 
     int m;
-    cout << "Enter no of edges: ";
-    cin >> m;
+    cout << "Enter number of edges: ";
+    cin >> m;   
 
     Graph g;
 
     for(int i=0; i<m; i++) {
         int u, v;
         cin >> u >> v;
-
+        // if 0 -> undirected or if 1 -> directed GRAPH
         g.addEdge(u, v, 0);
-    }
+    }    
 
+    cout << endl;
     g.print();
-    
+
     return 0;
 }
-
-/*
-1. create a map and store neighbours of every node in form of list
-*/
